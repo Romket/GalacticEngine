4777b8f6812f814b561277d68a8a2c7873e8ee27
leah-nyx <116913223+leah-nyx@users.noreply.github.com>
2026-01-15 08:10:22 -0700

    Update galactic.cpp to reflect license change

--- src/galactic.cpp
+++ src/galactic.cpp
@@ -7,15 +7,15 @@
  * 
  * This file is part of Galactic Engine.  Galactic Engine is free software:
  * you can redistribute it and/or modify it under the terms of the GNU
- * Affero General Public License as publishedby the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * Lesser General Public License as publishedby the Free Software Foundation,
+ * either version 2.1 of the License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * 
  */

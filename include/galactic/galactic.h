997cd6d1d04c39f79514938cbe997891e6f25d2b
leah-nyx <116913223+leah-nyx@users.noreply.github.com>
2026-01-15 08:11:45 -0700

    Fixed a typo

--- include/galactic/galactic.h
+++ include/galactic/galactic.h
@@ -9,7 +9,7 @@
  * 
  * This file is part of Galactic Engine.  Galactic Engine is free software:
  * you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as publishedby the Free Software Foundation,
+ * Lesser General Public License as published by the Free Software Foundation,
  * either version 2.1 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
